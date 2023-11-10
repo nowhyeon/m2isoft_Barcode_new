1d384615acf299dc8c5ae371eefae5c8db275af1
nowhyeon <wogus8873@gmail.com>
2023-11-10 17:44:26 +0900

    asdf

--- Barcode/m2i_Blood/My Project/Application.Designer.vb
+++ Barcode/m2i_Blood/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.m2i_Blood.frmLogin
+            Me.MainForm = Global.m2i_Blood.frmSplash
         End Sub
     End Class
 End Namespace
